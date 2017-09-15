b5237474aa7a18a6ad8e9a3b031e7b02cc5d2c40
Gergely Polonkai <gergely@polonkai.eu>
2017-09-15 14:12:10 +0200

    Initial version

---
+++ site/content/hello-world.rst
@@ -0,0 +1,15 @@
+Hello World!
+############
+
+:date: 2017-09-15 14:08:00
+:category: General
+
+Hello, World!
+
+We are the development team of getbenchmarked.io, trying to make the hiring process more fun to
+applicants, and easier to employees.
+
+This is our development blog.  You will find articles of our struggle with a lot of different
+software, and announcements of our publicly available libraries.
+
+Have fun reading!
---
+++ site/pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Devs of getbenchmarked.io'
+SITENAME = 'Developing benchmarks'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Budapest'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('getbenchmarked.io', 'https://getbenchmarked.io'),
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
